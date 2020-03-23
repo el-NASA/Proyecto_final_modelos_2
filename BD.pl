7dd0edf53e1bd8a93f561806b6a0f26f8e1e1f07
NASA <daniel_arod@protonmail.com>
2020-03-23 17:55:25 -0500

    se añadieron mas juegos, se están intentando agregar imagenes

--- BD.pl
+++ BD.pl
@@ -1,5 +1,8 @@
-juego(galaga_palomas,accion, 'https://bit.ly/2UaX8no',foto1,'descripcion 1').
-juego(galaga_palomas,disparos, 'https://bit.ly/2UaX8no',foto1,'descripcion 1').
-juego(covid_runner, plataformas, 'https://bit.ly/2UcROzN',foto2,'descripcion 2').
-juego(saltarin, plataformas, 'https://bit.ly/2J5QNDo',foto3,'descripcion 3').
-juego(saltarin, deporte, 'https://bit.ly/2J5QNDo',foto3,'descripcion 3').
+juego(galaga_palomas,accion, 'https://bit.ly/2UaX8no','galaga-palomas.PNG','juego al estilo del clasico Galaga, en el cual nuestro personaje tiene que evitar ser cagado por palomas...').
+juego(galaga_palomas,disparos, 'https://bit.ly/2UaX8no','galaga-palomas.PNG','juego al estilo del clasico Galaga, en el cual nuestro personaje tiene que evitar ser cagado por palomas...').
+juego(covid_runner, plataformas, 'https://bit.ly/2UcROzN',foto2,'Corre y salta para evitar un contagio del coronavirus...').
+juego(saltarin, plataformas, 'https://bit.ly/2J5QNDo',foto3,'juego al estilo de flappy-bird, en el cual nuestro personaje principal es mario \ny este tiene que evitar chocarse con los gigantes misiles!').
+juego(saltarin, deporte, 'https://bit.ly/2J5QNDo',foto3,'juego al estilo de flappy-bird, en el cual nuestro personaje principal es mario \ny este tiene que evitar chocarse con los gigantes misiles!').
+juego(busca_flores,puzzle,'https://bit.ly/2WCChuJ',foto4,'juego inspirado en el conocido buscaminas, pero tenemos que evitar tocar las flores').
+juego(caza_calaveras,accion,'https://bit.ly/2Ue2Zbu',foto5,'juego en el cual nuestro personaje principal tiene que perseguir y cazar todas las calaberas que pueda!').
+juego(caza_calaveras,aventura,'https://bit.ly/2Ue2Zbu',foto5,'juego en el cual nuestro personaje principal tiene que perseguir y cazar todas las calaberas que pueda!').
